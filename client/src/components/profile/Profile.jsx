bc3dc7c18966a7e13b37b3d0bedf7e26496868d5
gihooh <carloginocatapang@gmail.com>
2018-10-06 00:32:49 +0800

    Added additinal info for user

--- client/src/components/profile/Profile.jsx
+++ client/src/components/profile/Profile.jsx
@@ -3,14 +3,22 @@ import styles from './Profile.module.scss';
 
 class Profile extends React.PureComponent {
   render() {
-    const {location, name, picture} = this.props.details
+    const {location, name, picture, cell, gender, phone, email} = this.props.details
     return (
       <div className={styles.container}>
-        <img src={picture.large} alt="profile" className={styles.picture}/>
-        <div className={styles.details}>
-          <p>Name: {name.first} {name.last}</p>
-          <p>City: {location.city}</p>
-          <p>State: {location.state}</p>
+        <div className={`${styles.face} ${styles.faceFront}`}>
+         <img src={picture.large} alt="profile" className={styles.picture}/>
+         <div className={styles.details}>
+           <p>Name: {name.first} {name.last}</p>
+           <p>City: {location.city}</p>
+           <p>State: {location.state}</p>
+         </div>
+        </div>
+        <div className={`${styles.face} ${styles.faceBack}`}>
+          <p>Phone: {phone}</p>
+          <p>Email: {email}</p>
+          <p>Cell: {cell}</p>
+          <p>Gender: {gender}</p>
         </div>
       </div>
     )
